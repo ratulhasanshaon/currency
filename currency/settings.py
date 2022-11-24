d337cf3ef0ea58677ad314a9a8f703909d039bb6
ratulhasanshaon <ratulhasanshaon@gmail.com>
2022-11-24 12:27:03 +0600

    completed

--- currency/settings.py
+++ currency/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-eqb7^=j%26=9%s9!cq7s2%8%-i@-#xfv456d&6v07hkgmkihk2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
